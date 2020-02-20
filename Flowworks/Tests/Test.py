f9a97349f942771419af24c005193d2acadd1108
AlanLi1091 <alanli931108@hotmail.com>
2020-02-20 10:18:49 -0800

    did more tests

--- Flowworks/Tests/Test.py
+++ Flowworks/Tests/Test.py
@@ -2,15 +2,13 @@ import unittest
 import HtmlTestRunner
 from selenium import webdriver
 
-import os
-import sys
-import inspect
+import os, sys, inspect
 # fetch path to the directory in which current file is, from the root directory or C:\ (or whatever driver number it is)
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 # extract the path to parent directory
 parentdir = os.path.dirname(currentdir)
 # insert path to the folder from parent directory from which the python module/file to be imported
-sys.path.insert(0, parentdir)
+sys.path.insert(parentdir)
 
 
 from Resources.Locators import Locators
