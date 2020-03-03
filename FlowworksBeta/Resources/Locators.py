73437f9c1015ce03ee73dde45fb61dc3b16638ef
AlanLi1091 <alanli931108@hotmail.com>
2020-03-03 12:46:42 -0800

    added a test for beta sites

---
+++ FlowworksBeta/Resources/Locators.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.common.by import By
+
+class Locators():
+    INPUT_USERNAME_BOX=(By.ID, "txtUsername")
+    INPUT_PASSWORD_BOX=(By.XPATH, "//*[@id='dv-clientlogin']/div[2]/input")
+    LOGIN_BUTTON=(By.ID, "btnSubmitLogin")
+
+    AGREED_BOX=(By.ID, "Agreed")
+    CONTINUE_BUTTON=(By.ID, "continue")
+    #For graph tool access, tool bar locators
+    GRAPH_TOGGLE=(By.XPATH, "//*[@id='primary-navbar-collapse']/ul[1]/li[2]/a")
+    GRAPHING_TOOL=(By.XPATH, "//*[@id='primary-navbar-collapse']/ul[1]/li[2]/ul/li[1]/a")
+    #For generating graph, graphing tool locators
+    DC_CA=(By.XPATH, "//*[@id='598_anchor']")
+    DC_CAT=(By.XPATH, "//*[@id='598|185_anchor']")
+    DC_CAWS=(By.XPATH, "//*[@id='598|184_anchor']")
+    PLOT_BTN=(By.ID, "btnPlot")
+    GRAPH_CANVAS_LOCATOR = (By.ID, "graph-canvas")
+    #For accessing network map
+    MONITOR_TOGGLE=(By.XPATH, "//*[@id='primary-navbar-collapse']/ul[1]/li[1]/a")
+    NETWORK_MAP=(By.XPATH, "//*[@id='primary-navbar-collapse']/ul[1]/li[1]/ul/li[2]/a")
+    #For showing site map
+    DC_MAP=(By.XPATH, "//*[@id='598_anchor']")
+    MAP_PANEL_LOCATOR=(By.ID, "map-panel")
---
+++ FlowworksBeta/Resources/TestData.py
@@ -0,0 +1,10 @@
+import urllib3
+
+class TestData():
+    CHROME_EXECUTABLE_PATH = 'C:/Users/allan/Desktop/python-tcode/FWUITest/FlowworksBeta/Drivers/chromedriver.exe'
+    BASE_URL = "https://beta.flowworks.com"
+    USERNAME = 'alanli'
+    PASSWORD = "DC9kt10&2A8#"
+    HOME_PAGE_TITLE = "FlowWorks"
+    NETWORK_MAP_TITLE = "Network Map"
+    GRAPHING_TITLE = "Graphing"
