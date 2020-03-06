5b57b948ae5bee901f5245d5b934fef577abb6a1
AlanLi1091 <alanli931108@hotmail.com>
2020-03-06 15:28:54 -0800

    fixed some errors, completed the script.

--- FWBeta_test.py
+++ FWBeta_test.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 class CheckElementsCorrect(unittest.TestCase):
 
@@ -43,6 +44,7 @@ class CheckElementsCorrect(unittest.TestCase):
         for site in sites:
             site.click()
             self.assertTrue(google_map.is_displayed())
+            time.sleep(0.4)
         graph = self.driver.find_element_by_xpath("(/html/body/nav/div/div[2]/ul[1]/li[2]/a)")
         graph.click()
         graphing_tool = self.driver.find_element_by_xpath("(/html/body/nav/div/div[2]/ul[1]/li[2]/ul/li[1]/a)")
@@ -50,23 +52,40 @@ class CheckElementsCorrect(unittest.TestCase):
         self.assertIn("Graphing", self.driver.title)
         date_from = self.driver.find_element_by_id("txtFrom")
         date_from.clear()
-        date_from.send_keys("2006-01-01 00:00")
+        date_from.send_keys("2020-01-01 00:00")
         date_to = self.driver.find_element_by_id("txtTo")
         date_to.clear()
-        date_to.send_keys("2019-12-31 23:59")
+        date_to.send_keys("2020-02-29 23:59")
         allSideNodes = self.driver.find_elements_by_css_selector("#site-tree li[rel=site]")
         plot_data = self.driver.find_element_by_xpath("(/html/body/section/div/aside/div/div[2]/div[5]/button[1])")     
         allSideNodes_ids = []
         for i in range(0, len(allSideNodes)):
             allSideNodes_ids.append(allSideNodes[i].get_attribute("id"))
         for id in allSideNodes_ids:
+            button = self.driver.find_element_by_id("{}_anchor".format(str(id)))
+            button.click()
+            time.sleep(0.5)
             self.driver.execute_script("$('#site-tree').jstree('check_node',{})".format(str(id)))
-            WebDriverWait(self.driver, 2)
+            time.sleep(1)
             plot_data.click()
-            GRAPH_LOCATOR = (By.XPATH, "(/html/body/section/div/main/div[2]/div/div[2]/div/div/img)")
-            graph = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(GRAPH_LOCATOR))
-            self.assertTrue(graph.is_displayed())
-            self.driver.execute_script("$('#site-tree').jstree('uncheck_node',{})".format(str(id)))
+            GRAPH_CANVAS_LOCATOR = (By.ID, "graph-canvas")
+            ALERT = (By.CLASS_NAME, "alertify")
+            try:
+                WebDriverWait(self.driver, 1).until(EC.visibility_of_element_located(ALERT))
+                ok_button = self.driver.find_element_by_class_name("ok")
+                ok_button.click()
+                time.sleep(0.3)
+                self.driver.execute_script("$('#site-tree').jstree('uncheck_node',{})".format(str(id)))
+                button2 = self.driver.find_element_by_id("{}_anchor".format(str(id)))
+                button2.click()
+                time.sleep(0.8)
+            except TimeoutException:
+                graph_canvas = WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located(GRAPH_CANVAS_LOCATOR))
+                self.assertTrue(graph_canvas.is_displayed())
+                self.driver.execute_script("$('#site-tree').jstree('uncheck_node',{})".format(str(id)))
+                button2 = self.driver.find_element_by_id("{}_anchor".format(str(id)))
+                button2.click()
+                time.sleep(1)
         
     def tearDown(self):
         self.driver.quit()
